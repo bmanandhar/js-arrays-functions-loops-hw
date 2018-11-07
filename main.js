6c619eaa85d47e0c869c1affb2598b2f7dbe57a6
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-11-07 00:59:11 -0800

    home work done except bonus

--- main.js
+++ main.js
@@ -5,6 +5,10 @@
 
 var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 
+for (var i = 0; i < ages.length; i ++) {
+  console.log(ages[i]);
+}
+
 /*
 	Part 2: Let's create a new variable, "younger", which holds an empty array.
 	Using a for-loop, populate the "younger" array with numbers from the "ages"
@@ -13,6 +17,15 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	Hint: Use the array.push() method to add an element to the end of an array.
 	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
 */
+
+var younger = [];
+
+for (var i = 0; i < ages.length; i ++) {
+  if (ages[i] <= 20) {
+    younger.push(ages[i])
+  }
+}
+console.log(younger);
 
 /*
 	Part 3: We're comfortable going through an element from beginning to end, but let's
@@ -23,11 +36,30 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	at the end of the array?
 	Hint: What would the conditional for your for-loop look like?
 */
+var younger = [];
+
+for (var i = ages.length - 1; i >= 0; i --) {
+  if (ages[i] <= 20) {
+    younger.push(ages[i])
+  }
+}
+console.log(younger);
 
 /*
 	Part 4: Now that you've finished the first 3 parts using a for-loop, repeat the same exercises
 	(Part 1, Part 2, and Part 3) using a while-loop.
 */
+var younger = [];
+var i = 0;
+var younger = [];
+while (i <= ages.length) {
+  if (ages[i] <= 20) {
+    younger.push(ages[i]);
+  }
+  i ++
+}
+console.log(younger);
+
 
 /*
 	Part 5: Create a function, getDayOfWeek, that will tell you what day of the week it is.
@@ -35,6 +67,35 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	An invalid day of the week should return the string, "Please enter a valid day of the week".
 	Hint: Using a "switch" statement might result in cleaner code
 */
+var getDayOfWeek = function (dayNumber) {
+  switch (dayNumber) {
+    case 0:
+    day = "Sunday"; 
+    break;
+    case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+    break;
+  default:
+    alert("Please enter a valid day of the week");
+  }
+
+}
+getDayOfWeek(9);
 
 /*
 	Bonus - Part 6: All this work is making me hungry. Let's create a new function, addPepperoniToPizza,
@@ -49,3 +110,4 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	in our array?
 	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
 */
+
